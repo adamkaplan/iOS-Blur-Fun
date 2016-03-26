1e5811d21c437e93040809989a3e6bfa67bae4d2
Adam Kaplan <adamkaplan@yahoo-inc.com>
2016-03-26 15:15:28 -0400

    Initial Commit

---
+++ BlurViewPOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlurViewPOC
+//
+//  Created by Adam Kaplan on 3/26/16.
+//  Copyright © 2016 Yahoo, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlurViewPOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlurViewPOC
+//
+//  Created by Adam Kaplan on 3/26/16.
+//  Copyright © 2016 Yahoo, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
